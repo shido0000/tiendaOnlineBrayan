e08a3e4b6afc06ae2851fef86125ccdce73c30c0
jonathan <jonyperez1099@gmail.com>
2026-01-22 09:56:59 -0500

    actualizacion

--- Backend/fashionStore_back/API.Application/Program.cs
+++ Backend/fashionStore_back/API.Application/Program.cs
@@ -3,29 +3,74 @@ using API.Data.ClasesAuxiliares.Correo;
 using API.Domain.Services.NotificacionTiempoReal;
 using Microsoft.Extensions.FileProviders;
 
+//var builder = WebApplication.CreateBuilder(args);
+
+
+//// Add services to the container.
+//ConfigurationManager configuration = builder.Configuration;
+
+
+//builder.Services.AddRegistration(configuration); ;
+//IoCRegister.AddLogsRegistration(builder);
+
+//builder.Services.AddSignalR();
+
+
+//var app = builder.Build();
+//// Habilitar wwwroot como carpeta p�blica
+//app.UseStaticFiles();
+
+//// Startup.cs o Program.cs
+//app.MapHub<PedidosHub>("/pedidosHub");
+
+
+//IoCRegister.AddRegistration(app, app.Environment);
+
+//builder.Services.Configure<SmtpOptions>(builder.Configuration.GetSection("Smtp"));
+
 var builder = WebApplication.CreateBuilder(args);
 
+// Habilitar Kestrel y leer endpoints de appsettings.json
+builder.WebHost
+    .UseKestrel()
+    .UseConfiguration(builder.Configuration);
 
-// Add services to the container.
+// Add services
 ConfigurationManager configuration = builder.Configuration;
 
-
-builder.Services.AddRegistration(configuration); ;
+builder.Services.AddRegistration(configuration);
 IoCRegister.AddLogsRegistration(builder);
-
 builder.Services.AddSignalR();
 
-
 var app = builder.Build();
-// Habilitar wwwroot como carpeta p�blica
+
+// Archivos estáticos
 app.UseStaticFiles();
 
-// Startup.cs o Program.cs
+// SignalR
 app.MapHub<PedidosHub>("/pedidosHub");
 
+app.Use(async (context, next) =>
+{
+    await next();
 
+    // Si el resultado es 404 y no es una API ni un archivo estático
+    if (context.Response.StatusCode == 404 &&
+        !context.Request.Path.Value.StartsWith("/api") &&
+        !System.IO.Path.HasExtension(context.Request.Path.Value))
+    {
+        context.Response.StatusCode = 200;
+        await context.Response.SendFileAsync(Path.Combine(app.Environment.WebRootPath, "index.html"));
+    }
+});
+
+// Rutas de tu API
 IoCRegister.AddRegistration(app, app.Environment);
 
+// SMTP
 builder.Services.Configure<SmtpOptions>(builder.Configuration.GetSection("Smtp"));
+
+app.Run();
+
 
 
---
+++ Backend/fashionStore_back/API.Application/wwwroot/assets/QSpace.2edd7412.js
@@ -0,0 +1,1 @@
+import{h as e,G as a}from"./index.c80a0157.js";const p=e("div",{class:"q-space"});var c=a({name:"QSpace",setup(){return()=>p}});export{c as Q};
